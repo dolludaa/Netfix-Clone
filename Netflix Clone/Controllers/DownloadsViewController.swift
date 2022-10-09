b154aadc09b6c23fba2df2fe594e08007afdbc8d
dolludaa <dolontaeva00@gmail.com>
2022-10-09 04:16:51 +0300

    MainTabBarViewController

---
+++ Netflix Clone/Controllers/DownloadsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  DownloadsViewController.swift
+//  Netflix Clone
+//
+//  Created by Людмила Долонтаева on 09.10.2022.
+//
+
+import UIKit
+
+class DownloadsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+
+        
+    }
+    
+
+ 
+
+}
---
+++ Netflix Clone/Controllers/UpcomingViewController.swift
@@ -0,0 +1,20 @@
+//
+//  UpcomingViewController.swift
+//  Netflix Clone
+//
+//  Created by Людмила Долонтаева on 09.10.2022.
+//
+
+import UIKit
+
+class UpcomingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+
+    }
+    
+
+
+}
